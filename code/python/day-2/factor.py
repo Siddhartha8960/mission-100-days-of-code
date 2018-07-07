35c4aec164ded1bc09ccd74d76946c8d5663f0cc
Siddhartha Singh <40606932+Siddhartha8960@users.noreply.github.com>
2018-07-07 18:36:09 +0530

    day-2 code

---
+++ code/python/day-2/factor.py
@@ -0,0 +1,5 @@
+num = int(input("enter any number "))
+n=num
+for i in range(1,n+1):
+    if n%i==0:
+        print(i)
---
+++ code/python/day-2/temperature.py
@@ -0,0 +1,4 @@
+celsius = int(input("Enter the temperature in celcius "))
+c=celsius
+Fahrenheit = (1.8*c)+32
+print(Fahrenheit, "is the required temperature")
